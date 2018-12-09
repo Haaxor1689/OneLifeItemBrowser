12d22e6d787544d00bff2aad389afd7b9bdba5a4
Haaxor1689 <betkomaros@gmail.com>
2018-12-10 00:04:52 +0100

    Addet few traits into oject list.

--- src/Components/RecordListItemComponent.tsx
+++ src/Components/RecordListItemComponent.tsx
@@ -12,7 +12,13 @@ export default class RecordListItemComponent extends React.Component<IObjectReco
             <tr onClick={() => this.props.onSelected(this.props.objectRecord)}>
                 <th scope="row">{this.props.objectRecord.id}</th>
                 <td>{this.props.objectRecord.description}</td>
-                <td>{this.props.objectRecord.containable ? this.props.objectRecord.containSize : "\u221e"}</td>
+                <td>{this.props.objectRecord.displaySize()}</td>
+                <td>
+                    {this.props.objectRecord.isEdible() && <span className="glyphicon glyphicon-apple"></span>}
+                    {this.props.objectRecord.isClothing() && <span className="glyphicon glyphicon-sunglasses"></span>}
+                    {this.props.objectRecord.rideable && <span className="glyphicon glyphicon-plane"></span>}
+                    {this.props.objectRecord.permanent && <span className="glyphicon glyphicon-tower"></span>}
+                </td>
                 <td>Placeholder actions</td>
             </tr>
         );
