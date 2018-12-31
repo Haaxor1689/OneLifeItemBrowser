0fb43516152b1eec72a403d43713b02ff150434b
Haaxor1689 <betkomaros@gmail.com>
2018-12-31 15:41:32 +0100

    Improved object record component

--- src/Components/ObjectRecordComponent.tsx
+++ src/Components/ObjectRecordComponent.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 
 import ObjectRecord from 'src/Models/ObjectRecord';
 import Sprite from 'src/Models/Sprite';
+import KeyValueComponent from './KeyValueComponent';
 
 var tgaLib = require("../Lib/tga");
 const imgUrl = "https://raw.githubusercontent.com/jasonrohrer/OneLifeData7/master/sprites/";
@@ -29,20 +30,24 @@ export default class ObjectRecordComponent extends React.Component<IObjectRecord
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col">
-                        <h2>{this.props.objectRecord.description}</h2>
-                    </div>
-                    <div className="w-100"></div>
-                    <button type="button" className="btn btn-outline-dark" onClick={this.props.close}>Close</button>
                     <div className="col-md-10">
-                        <p>{this.props.objectRecord.id}</p>
+                        <div className="row">
+                            <button type="button" className="btn btn-link" onClick={this.props.close}>&lt; Back to listing</button>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <h2>{this.props.objectRecord.description}</h2>
+                                <KeyValueComponent keyStr="Id" value={this.props.objectRecord.id} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <img src="https://via.placeholder.com/120"/>
                     </div>
                 </div>
                 <div className="row">
-                    <h3>Sprites:</h3>
-                </div>
-                <div className="row">
                     <div className="col">
+                        <h3>All sprites:</h3>
                         {this.props.objectRecord.sprites.map((sprite) => <canvas id={this.props.objectRecord.id + "_" + sprite.id}/>)}
                     </div>
                 </div>
